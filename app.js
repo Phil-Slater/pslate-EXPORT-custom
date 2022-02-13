69a8db533d89019a0935f0f85547d8fc950c80e1
Phil-Slater <pdsltr@gmail.com>
2022-02-13 13:13:06 -0500

    install dependencies and server logging

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import { Shopify } from '@shopify/shopify-api';
-dotenv.config();
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const logger = require('morgan');
+const debug = require('debug')('pslateexport:server');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const { Shopify } = require('@shopify/shopify-api')
 
-const host = '127.0.0.1';
+require('dotenv').config();
+
 const port = 3000;
+
+app.set('port', port);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+app.engine('mustache', mustacheExpress('./views/partials', '.mustache'));
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST } = process.env
 
@@ -14,17 +32,15 @@ Shopify.Context.initialize({
     API_KEY: SHOPIFY_API_KEY,
     API_SECRET_KEY: SHOPIFY_API_SECRET,
     SCOPES: SHOPIFY_API_SCOPES,
-    HOST_NAME: HOST,
+    HOST_NAME: HOST.replace(/https:\/\//, ""),
     IS_EMBEDDED_APP: true,
 });
 
-const app = express();
-
-
+//typeof shops[req.query.shop] !== 'undefined'
 app.get('/', async (req, res) => {
-    if (typeof shops[req.query.shop] !== 'undefined') {
+    if (shops !== {}) {
         console.log(req.query.shop)
-        res.send('Hello World!');
+        res.send('Hello world/pslate!!!!!!!!!!!');
     } else {
         res.redirect(`/auth?shop=${req.query.shop}`);
     }
@@ -35,24 +51,50 @@ app.get('/auth', async (req, res) => {
         req,
         res,
         req.query.shop,
-        '/auth/callback',
-        true,
+        '/auth/shopify/callback',
+        false,
     )
     res.redirect(authRoute);
 });
 
-app.get('/auth/callback', async (req, res) => {
+app.get('/auth/shopify/callback', async (req, res) => {
     const shopSession = await Shopify.Auth.validateAuthCallback(
         req,
         res,
         req.query
     );
-    console.log(shopSession);
+    //console.log(shopSession);
     shops[shopSession.shop] = shopSession;
+    //console.log(shops);
     res.redirect(`https://${shopSession.shop}/admin/apps/pslate-export`);
 });
 
+// error handler
+function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
 
-app.listen(port, () => {
-    console.log(`Server is running at http://${host}:${port}`);
-});
+    const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+}
+
+// console server logging
+function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+}
